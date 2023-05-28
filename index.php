4f309efe2958e70f226844379a1f3ef933a2ea17
Jorge Saldivar <14043861+jsldvr@users.noreply.github.com>
2023-05-28 12:18:42 -0500

    set img max width

--- index.php
+++ index.php
@@ -28,6 +28,12 @@ $app = [
     <link href="https://cdn.datatables.net/v/bs5/jq-3.6.0/dt-1.13.4/datatables.min.css" 
         rel="stylesheet" />
 
+    <style>
+        img {
+            max-width: 100%;
+        }
+    </style>
+
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
